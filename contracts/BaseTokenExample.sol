7e634896d0c0e61cdfeb8790e3594dd7e83ca964
AmzCryptoTech <xrob77@gmail.com>
2023-11-01 06:53:27 +0100

    base token example

--- contracts/BaseTokenExample.sol
+++ contracts/BaseTokenExample.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "./BaseFactoryToken.sol";
+import "./InitBaseToken.sol";
 
-contract BaseFactoryTokenExample is BaseFactoryToken {
+contract BaseTokenExample is InitBaseToken {
 
-   constructor() BaseFactoryToken(
+   constructor() InitBaseToken(
+    _msgSender(),
     "NFT Test", 
     "NFT", 
     "ipfs://bafybeigi65dui5cikqizz4qpxwighq6sw66w4xavoqu3enoxgheckybt34",
     1000, 
     0) {
+
     }
 
 }
